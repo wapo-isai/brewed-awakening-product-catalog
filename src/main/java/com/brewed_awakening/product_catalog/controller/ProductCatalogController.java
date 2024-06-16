eef3e5d381232a28dc264091a697913389a9851b
wapo-isai <wapinho513@hotmail.com>
2024-06-16 12:29:57 -0400

    setup product controller for client consumption

--- src/main/java/com/brewed_awakening/product_catalog/controller/ProductCatalogController.java
+++ src/main/java/com/brewed_awakening/product_catalog/controller/ProductCatalogController.java
@@ -2,30 +2,21 @@ package com.brewed_awakening.product_catalog.controller;
 
 import com.brewed_awakening.product_catalog.dto.ProductItem;
 import com.brewed_awakening.product_catalog.repository.ProductItemRepository;
-//import com.brewed_awakening.product_catalog.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/product-catalog/")
 public class ProductCatalogController {
     private final ProductItemRepository productItemRepository;
-//    private final S3Service s3Service;
 
-//    @Autowired
-//    public ProductCatalogController(ProductItemRepository productItemRepository, S3Service s3Service) {
-//        this.productItemRepository = productItemRepository;
-//        this.s3Service = s3Service;
-//    }
-@Autowired
-public ProductCatalogController(ProductItemRepository productItemRepository) {
-    this.productItemRepository = productItemRepository;
+    @Autowired
+    public ProductCatalogController(ProductItemRepository productItemRepository) {
+        this.productItemRepository = productItemRepository;
 
-}
+    }
 
     @GetMapping("/product-list")
     public List<ProductItem> getProductList() {
@@ -34,22 +25,13 @@ public ProductCatalogController(ProductItemRepository productItemRepository) {
         return productItems;
     }
 
-//    @PostMapping("/uploadImage/{id}")
-//    public ProductItem uploadProductImage(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
-//        try {
-//            ProductItem productItem = productItemRepository.findById(id).orElseThrow(() -> new Exception("Product not found"));
-//            String imageUrl = s3Service.uploadFile(file);
-//            productItem.setImageUrl(imageUrl);
-//            return productItemRepository.save(productItem);
-//        } catch (Exception e) {
-//
-//        }
-//
-//        return null;
-//    }
-
     @PostMapping("/saveItem")
     public ProductItem saveProductItem(@RequestBody ProductItem productItem){
         return productItemRepository.save(productItem);
+    }
+
+    @GetMapping("/{productId}")
+    public ProductItem getProduct(@PathVariable("productId") long productId){
+        return productItemRepository.findById(productId);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 170, 'A light roast coffee with citrusy and floral notes.', 'Sunrise Roast' ,8);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 220, 'A rich, creamy blend of espresso and dark chocolate.', 'Velvet Mocha' , 10);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 180, 'Coffee with a touch of honey and almond milk.', 'Honey Almond Brew' , 10);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 250, 'Espresso with steamed milk, toffee, and nutty flavors.', 'Toffee Nut Latte' , 8);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 150, 'A classic, strong shot of espresso, perfect for late nights.', 'Midnight Espresso' , 12);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 200, 'A frothy macchiato with a swirl of caramel.', 'Caramel Cloud Macchiato' , 9);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 230, 'A cappuccino with maple syrup and pecan flavor.', 'Maple Pecan Cappuccino' , 8);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 190, 'Cold brew coffee with coconut cream and a hint of vanilla.', 'Coconut Cream Cold Bre' , 13);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 150, 'Coffee with saffron, cardamom, and a dash of cinnamon.', 'Saffron Spiced Coffee' , 11);
+
+INSERT INTO product_item (image_url, product_calories, product_description, product_name, product_price) VALUES ('https://brewed-awakening-coffee-shop.s3.amazonaws.com/sunrise-roast.webp', 240, 'A unique mix of chai spices and hazelnut-flavored coffee.', 'Hazelnut Chai Fusion' , 9);
